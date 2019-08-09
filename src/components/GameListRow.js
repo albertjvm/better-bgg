f4a9e0e06a1f4d4b258991608e8d92b5a28179d8
Albert VanderMeulen <albertjvm@users.noreply.github.com>
2019-08-09 09:01:26 -0400

    add playtime to gamelistrow

--- src/components/GameListRow.js
+++ src/components/GameListRow.js
@@ -4,7 +4,16 @@ import ReactSVG from 'react-svg';
 import { ListRow } from './ListRow';
 
 import meeple from '../icons/meeple.svg';
+import clock from '../icons/clock.svg';
 import './GameListRow.css';
+
+function playerString({ minplayers, maxplayers }) {
+  return `${minplayers}${minplayers === maxplayers ? '' : ` - ${maxplayers}`}`;
+}
+
+function playtimeString({ minplaytime, maxplaytime }) {
+  return `${minplaytime}${minplaytime === maxplaytime ? '' : ` - ${maxplaytime}`}`;
+}
 
 export function GameListRow({ game, index }) {
   return (
@@ -13,7 +22,11 @@ export function GameListRow({ game, index }) {
         <span className="GameListRow-name">{game.name}</span>
         <span className="GameListRow-players">
           {game.minplayers ? <ReactSVG className="GameListRow-players-icon" src={meeple} /> : null}
-          {game.minplayers ? `${game.minplayers} - ${game.maxplayers}`: ''}
+          {game.minplayers ? `${playerString(game)}`: ''}
+        </span>
+        <span className="GameListRow-playtime">
+          {game.minplaytime ? <ReactSVG className="GameListRow-playtime-icon" src={clock} /> : null}
+          {game.minplaytime ? `${playtimeString(game)}`: ''}
         </span>
       </div>
     </ListRow>
